0177b5f33f31b154a1ebc197a8fd32a879b17a82
Chris Walston <Chris@myaccountant.local>
2020-12-28 09:26:23 -0700

    worked on contact and portfolio pages

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,15 +4,15 @@ const ContactForm = () => {
   
   return (
     <section>
+      <h2 class="page-title">Contact Me</h2>
       <div className="form-wrapper">
-        <h2 class="page-title">Contact Me</h2>
         <form>
           <label for="firstName">Full Name: </label>
-          <input id="firstName" className="border-radius"></input>
+          <input id="firstName" className="border-radius" type="text"></input>
           <label for="email">Email Address: </label>
-          <input id="email" className="border-radius"></input>
+          <input id="email" className="border-radius" type="text"></input>
           <label for="contact-text">Message:</label>
-          <textarea id="contact-text" className="border-radius"></textarea>
+          <textarea id="contact-text" className="border-radius" type="text"></textarea>
           <button type="submit" className="border-radius">Submit</button>
         </form>
       </div>
