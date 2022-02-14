7a9dcc0b1b7173fcb6e985780bba4e7305aba5fc
chrisrw300 <chrisrw300@gmail.com>
2022-02-13 22:58:28 -0700

    more styling, vertical portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -8,47 +8,55 @@ const Portfolio = () => {
     return (
         <section className="portfolio-wrapper" id="portfolio">
             <h2>Portfolio</h2>
-            <h3>Front End Projects</h3>
-            <div className="cards-wrapper">
-                {frontEnd.map(portfolio => (
-                    <PortCard
-                    id={portfolio.id}
-                    key={portfolio.key}
-                    name={portfolio.name}
-                    image={portfolio.image}
-                    github={portfolio.github}
-                    deployed={portfolio.deployed}
-                />
-                ))}
-            </div>
 
-            <h3>Back End Projects</h3>
-            <div className="cards-wrapper">
-                {backEnd.map(portfolio => (
-                    <PortCard
-                    id={portfolio.id}
-                    key={portfolio.key}
-                    name={portfolio.name}
-                    image={portfolio.image}
-                    github={portfolio.github}
-                    deployed={portfolio.deployed}
-                />
-                ))}
+            <div class="portfolio-section ps1">
+                <h3>Front End Projects</h3>
+                <div className="cards-wrapper">
+                    {frontEnd.map(portfolio => (
+                        <PortCard
+                        id={portfolio.id}
+                        key={portfolio.key}
+                        name={portfolio.name}
+                        image={portfolio.image}
+                        github={portfolio.github}
+                        deployed={portfolio.deployed}
+                    />
+                    ))}
+                </div>
             </div>
-
-            <h3>Full Stack Projects</h3>
-            <div className="cards-wrapper">
-                {fullStack.map(portfolio => (
-                    <PortCard
-                    id={portfolio.id}
-                    key={portfolio.key}
-                    name={portfolio.name}
-                    image={portfolio.image}
-                    github={portfolio.github}
-                    deployed={portfolio.deployed}
-                />
-                ))}
+            
+            <div class="portfolio-section ps2">
+                <h3>Back End Projects</h3>
+                <div className="cards-wrapper">
+                    {backEnd.map(portfolio => (
+                        <PortCard
+                        id={portfolio.id}
+                        key={portfolio.key}
+                        name={portfolio.name}
+                        image={portfolio.image}
+                        github={portfolio.github}
+                        deployed={portfolio.deployed}
+                    />
+                    ))}
+                </div>
             </div>
+            
+            <div class="portfolio-wrapper ps3">
+                <h3>Full Stack Projects</h3>
+                <div className="cards-wrapper">
+                    {fullStack.map(portfolio => (
+                        <PortCard
+                        id={portfolio.id}
+                        key={portfolio.key}
+                        name={portfolio.name}
+                        image={portfolio.image}
+                        github={portfolio.github}
+                        deployed={portfolio.deployed}
+                    />
+                    ))}
+                </div>
+            </div>
+            
         </section>
     )
 }
